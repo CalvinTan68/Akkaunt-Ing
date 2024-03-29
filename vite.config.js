b69d04e2a87bc9133f385853949b03a761a858de
Calvin Tandika <calvintandikaa68@gmail.com>
2024-03-29 10:19:14 +0700

    New package and sitemap config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
 import laravel from "laravel-vite-plugin";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
     plugins: [
         laravel(["resources/js/app.jsx", "resources/css/app.css"]),
         react(),
+        splitVendorChunkPlugin(),
+        Sitemap({
+            hostname: "https://akkaunt-ing.rf.gd",
+            outDir: "public",
+        }),
     ],
     resolve: {
         alias: {
