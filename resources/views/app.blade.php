24ae7860d1c0a5b052ffd551d6f8dd2567a580a2
Calvin Tandika <calvintandikaa68@gmail.com>
2024-03-30 07:57:49 +0700

    Adapting to new config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
     <meta name="author" content="Calvin Tandika" />
+    <meta name="title" content="AKKAUNT-ING" />
+    <title>AKKAUNT-ING</title>
     <meta name="description" content="A simple yet useful bookkeeping app" />
-    <link rel="canonical" href="{{ url()->current() }}" />
-
+    <link rel="canonical" href="{{ url('') }}" />
     @routes
     @viteReactRefresh
     @vite('resources/js/app.jsx')
@@ -15,7 +16,7 @@
     @inertiaHead
 </head>
 
-<body class="font-sans antialiased" style="margin:0; background: #f5f5f5 !important">
+<body class="font-sans antialiased" style="margin: 0 !important;background: #f5f5f5 !important;">
     @inertia
 </body>
 
