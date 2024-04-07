8a5e9323de7cc4b46bc9424a9156da6150dcab89
Calvin Tandika <calvintandikaa68@gmail.com>
2024-04-07 16:34:46 +0700

    Re-config vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import laravel from "laravel-vite-plugin";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
     plugins: [
         laravel(["resources/js/app.jsx", "resources/css/app.css"]),
         react(),
-        splitVendorChunkPlugin(),
         Sitemap({
             hostname: "https://akkaunt-ing.rf.gd",
             outDir: "public",
@@ -16,25 +15,6 @@ export default defineConfig({
     resolve: {
         alias: {
             "@": "/resources/js",
-        },
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes("@ant-design/icons")) {
-                        return "@icons";
-                    } else if (id.includes("axios")) {
-                        return "@axios";
-                    } else if (id.includes("chart.js")) {
-                        return "@chart-js";
-                    } else if (id.includes("dayjs")) {
-                        return "@dayjs";
-                    } else if (id.includes("react-chartjs-2")) {
-                        return "@chart-js-react";
-                    }
-                },
-            },
         },
     },
 });
